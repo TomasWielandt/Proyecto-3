003f360be54ec2f33c106d87fa151f44a4e17e28
TomasWielandt <twielandt16@gmail.com>
2024-06-06 17:44:07 -0400

    se arregló comentarios en js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-// prevención del envio del formulario de contacto
+// prevención del botón de busqueda
 document.getElementById("busqueda").addEventListener("click", (event) => {
     event.preventDefault();
     alert("El botón de búsqueda no está funcionando en este momento");
 });
 
+// prevención del envio del formulario de contacto
 document.getElementById("formulario").addEventListener("submit", (event) => {
     event.preventDefault();
     alert("El formulario no está funcionando en este momento");
